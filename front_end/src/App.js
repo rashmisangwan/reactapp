d32df2a459df11a6275b07a258233c3e2930b70e
rashmisangwan <errashmisangwan@gmail.com>
2018-07-25 18:37:51 +0530

    modified src/App.js showing data in form of table instead of list

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,9 +1,82 @@
+// import React, { Component } from 'react'
+// import { Form } from 'semantic-ui-react'
+
+// import 'react-virtualized/styles.css'
+
+// import { List } from 'react-virtualized';
+
+
+// class App extends Component {
+//   state = {}
+//   handleInputChange = (e, {value}) => {
+//   	this.setState({username: value})
+//   }
+
+//   handleChange = (e) => {
+//   	e.preventDefault()
+//   	fetch('https://api.github.com/users/' + this.state.username + '/repos')
+// 	  	.then(res => res.json())
+// 	  	.then(res => {
+// 	  		this.setState({'repos': res})
+// 	  	})
+// 	  	.catch(err => {
+// 	  		console.log(err);
+// 	  	})
+//   	}
+
+//   rowRenderer = ({
+// 	  key,
+// 	  index,
+// 	  isScrolling,
+// 	  isVisible,
+// 	  style
+// 	}) => {
+//   	let repo = this.state.repos[index]
+//   	console.log(repo)
+//   	return (
+// 	    <div
+// 	      key={key}
+// 	      style={style}
+// 	    >
+// 	    	<a href={repo.html_url}>
+// 	    		{repo.name}
+// 	    	</a>
+// 	    </div>
+// 	  )
+//   }
+
+//   render() {
+//   	let repos = this.state.repos || []
+
+//     return (
+//     	<div>
+// 	      <Form onSubmit = {this.handleChange}>
+// 	        <Form.Input fluid label='username' placeholder='username' name='username' onChange={this.handleInputChange} />
+// 	        <Form.Button>Submit</Form.Button>
+// 	      </Form>
+// 	      <List
+// 			    width={700}
+// 			    height={300}
+// 			    rowCount={repos.length}
+// 			    rowHeight={20}
+// 			    rowRenderer={this.rowRenderer}
+// 			  />
+//     	</div>
+//     )
+//   }
+// }
+
+// export default App
+
+
+
+
 import React, { Component } from 'react'
 import { Form } from 'semantic-ui-react'
 
 import 'react-virtualized/styles.css'
 
-import { List } from 'react-virtualized';
+import { Column, Table } from 'react-virtualized';
 
 
 class App extends Component {
@@ -24,27 +97,6 @@ class App extends Component {
 	  	})
   	}
 
-  rowRenderer = ({
-	  key,
-	  index,
-	  isScrolling,
-	  isVisible,
-	  style
-	}) => {
-  	let repo = this.state.repos[index]
-  	console.log(repo)
-  	return (
-	    <div
-	      key={key}
-	      style={style}
-	    >
-	    	<a href={repo.html_url}>
-	    		{repo.name}
-	    	</a>
-	    </div>
-	  )
-  }
-
   render() {
   	let repos = this.state.repos || []
 
@@ -54,13 +106,30 @@ class App extends Component {
 	        <Form.Input fluid label='username' placeholder='username' name='username' onChange={this.handleInputChange} />
 	        <Form.Button>Submit</Form.Button>
 	      </Form>
-	      <List
-			    width={700}
+	      <Table
+			    width={1300}
 			    height={300}
+			    headerHeight={40}
+			    rowHeight={30}
 			    rowCount={repos.length}
-			    rowHeight={20}
-			    rowRenderer={this.rowRenderer}
-			  />
+			    rowGetter={({ index }) => repos[index]}
+  			>
+			    <Column
+			      label='Name'
+			      dataKey='name'
+			      width={300}
+			    />
+			    <Column
+			      width={300}
+			      label='Description'
+			      dataKey='description'
+			    />
+			    <Column
+			    	width = {300}
+			    	label= 'nodeid'
+			    	dataKey = 'node_id'
+			    />
+  			</Table>
     	</div>
     )
   }
